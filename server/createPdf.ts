702ae248d194475aa634762d496e5359cec58fe7
dhythm <yuta.okada.20@gmail.com>
2023-03-20 17:35:11 +0900

    update

--- server/createPdf.ts
+++ server/createPdf.ts
@@ -1,15 +1,17 @@
 import { memoryUsage } from "node:process";
-import ReactPDF, { renderToFile } from "@react-pdf/renderer";
+import ReactPDF, { renderToFile, renderToStream } from "@react-pdf/renderer";
 import { SamplePdf } from "../src/SamplePdf";
 import { Command } from "commander";
+import { Base64Encode } from "base64-stream";
 
 const VOLUME = 10_000;
 
 const program = new Command();
-program.parse(process.argv);
+program.option("-s, --stream", "use stream").parse(process.argv);
+const options = program.opts();
 const COUNT_ROWS = Number(program.args[0] ?? VOLUME);
 
-const main = async () => {
+const toFile = async () => {
   const fileName = `${__dirname}/sample.pdf`;
   await renderToFile(
     SamplePdf({
@@ -17,13 +19,50 @@ const main = async () => {
     }),
     fileName
   );
-  console.log("It works!");
+};
+
+const toStream = async () => {
+  const stream = await renderToStream(
+    SamplePdf({
+      data: [...Array(COUNT_ROWS)].map((_, idx) => `${idx + 1}`),
+    })
+  );
+  const promise = () => {
+    let data = "";
+    return new Promise((resolve, reject) => {
+      stream
+        .pipe(new Base64Encode())
+        .on("data", (chunk) => {
+          data += chunk;
+        })
+        .on("end", () => {
+          resolve(data);
+        })
+        .on("error", (error) => reject(error));
+    });
+  };
+  const base64str = await promise();
 };
 
 (async () => {
   const start = process.hrtime();
-  await main();
+  if (options.stream) {
+    console.log("mode: stream");
+    await toStream();
+  } else {
+    console.log("mode: file");
+    await toFile();
+  }
   const end = process.hrtime(start);
-  console.log(memoryUsage());
+  const usages = memoryUsage();
+  const messages = [];
+  console.log(
+    Object.entries(usages)
+      .map(
+        ([key, value]) =>
+          `${key}: ${Math.round((value / 1024 ** 2) * 100) / 100} MB`
+      )
+      .join(", ")
+  );
   console.log({ rows: COUNT_ROWS, start, end });
 })();
