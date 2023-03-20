3460a96ec5e4985332fff390f6aa51f3fd8071a2
dhythm <yuta.okada.20@gmail.com>
2023-03-20 11:51:01 +0900

    update

--- server/createPdf.ts
+++ server/createPdf.ts
@@ -1,15 +1,22 @@
+import { memoryUsage } from "node:process";
 import ReactPDF, { renderToFile } from "@react-pdf/renderer";
 import { SamplePdf } from "../src/SamplePdf";
+
+const VOLUME = 1000;
 
 const main = async () => {
   const fileName = `${__dirname}/sample.pdf`;
   await renderToFile(
-    SamplePdf({ data: [...Array(35)].map((_, idx) => `${idx + 1}`) }),
+    SamplePdf({ data: [...Array(VOLUME)].map((_, idx) => `${idx + 1}`) }),
     fileName
   );
   console.log("It works!");
 };
 
 (async () => {
+  const start = process.hrtime();
   await main();
+  const end = process.hrtime(start);
+  console.log(memoryUsage());
+  console.log({ rows: VOLUME, start, end });
 })();
